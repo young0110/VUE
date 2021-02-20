3319640969993e6d810f29b7d20efd6c31c53bf3
young <jillson0110@outlook.com>
2021-02-20 15:36:57 +0800

    [*]登录页样式修改

--- vue-demo/src/components/common/Login.vue
+++ vue-demo/src/components/common/Login.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="login-main">
-    <div class="win left-win"></div>
-    <div class="win right-win">
-      <div class="login-block">
+  <div class="main">
+    <!-- 左边模块 -->
+    <div class="login-page login-page-left"></div>
+    <!-- 右边模块 -->
+    <div class="login-page login-page-right">
+      <div class="login-bar">
         <p>Login</p>
-        <el-form label-width="100px">
-          <el-form-item label="用户名">
-            <el-input auto-complete="off" v-model="loginForm.userName"/>
+        <el-form class="login-win" label-width="100px">
+          <el-form-item class="login-win-line" label="用户名" >
+            <el-input auto-complete="off" v-model="loginForm.userName" placeholder="请输入用户名" />
           </el-form-item>
-          <el-form-item label="密码">
-            <el-input type="password" auto-complete="off" v-model="loginForm.password" />
+          <el-form-item class="login-win-line" label="密码" >
+            <el-input type="password" auto-complete="off" v-model="loginForm.password" placeholder="请输入密码" />
           </el-form-item>
-          <el-form-item>
+          <el-form-item class="login-win-line">
             <el-button @click="login">登录</el-button>
           </el-form-item>
         </el-form>
@@ -34,69 +36,78 @@ export default {
   methods: {
     login () {
       let userName = this.loginForm.userName
-      this.$store.commit('login', userName)
-      this.$router.push('/home')
+      let password = this.loginForm.password
+      if (userName && password) {
+        // TODO 验证用户名密码
+        this.$store.commit('login', userName)
+        this.$router.push('/home')
+      } else {
+        this.$message.warning('请输入用户名和密码！')
+      }
     }
   }
 }
 </script>
 
-<style lang="less">
-.login-main {
-  width: 100%;
-  height: 100%;
+<style lang="less" scoped>
+.main {
   display: flex;
-  .win {
+  .login-page {
     display: inline-block;
   }
-  .left-win {
+  .login-page-left {
     width: 60%;
     height: 100%;
-    background: #ccc;
   }
-  .right-win {
+  .login-page-right {
     width: 40%;
     height: 100%;
-    .login-block {
+    .login-bar {
       width: 480px;
       height: auto;
-      margin: 240px auto 0;
-      background: #eee;
-      padding: 50px 0;
+      min-height: 360px;
+      position: relative;
+      top: 240px;
+      left: calc(50% - 240px);
+      padding: 40px 0;
       p {
         height: 40px;
-        padding: 10px;
+        line-height: 40px;
+        font-weight: bold;
+        padding: 10px 40px;
         font-size: 20px;
       }
-      .el-form-item {
-        margin: 30px auto 0;
-        .el-input {
-          width: 300px;
-          border-bottom: 2px solid #ccc;
-        }
-        .el-button {
-          width: 300px;
-          background: #ccc;
-        }
-        .el-input input:hover {
-          border: 0;
-          outline: 0;
-        }
-        .el-input input:focus-within {
-          border: 0;
-          outline: 0;
-        }
-        .el-input:hover {
-          border-bottom: 2px solid #999;
-        }
-        .el-button:hover {
-          background: #999;
-          border: 0;
-          color: #fff;
+      .login-win {
+        padding: 10px 0;
+        .login-win-line {
+          margin: 30px auto 0;
+          .el-input {
+            width: 300px;
+          }
+          .el-button {
+            width: 300px;
+            font-weight: bold;
+          }
+          .el-input input {
+            border-radius: 4px;
+          }
+          .el-input input:hover {
+            border: 0;
+            outline: 0;
+          }
+          .el-input input:focus-within {
+            border: 0;
+            outline: 0;
+          }
+          .el-input:hover {
+
+          }
+          .el-button:hover {
+            border: 0;
+          }
         }
       }
     }
   }
-
 }
 </style>
